563607f76c2e3960cfa6cbf3d5126a0c9f3b0717
Raushan Kumar <raushan376kumar@gmail.com>
2022-09-28 00:18:29 +0530

    ad

---
+++ OnlineTripManagementSystemApplication/src/main/java/app/trip/OnlineTripManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package app.trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTripManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTripManagementSystemApplication.class, args);
+	}
+
+}
---
+++ OnlineTripManagementSystemApplication/src/test/java/app/trip/OnlineTripManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package app.trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTripManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
