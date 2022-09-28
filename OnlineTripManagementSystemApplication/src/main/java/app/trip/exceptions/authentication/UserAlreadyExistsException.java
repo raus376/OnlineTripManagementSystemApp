ee554b032544ab05952ad23819f8ccb013faed84
Sumit-Gangwar <gangwarsumit999@gmail.com>
2022-09-29 00:21:35 +0530

    Customer SignUp, SignIn, Logout

---
+++ OnlineTripManagementSystemApplication/src/main/java/app/trip/exceptions/authentication/UserAlreadyExistsException.java
@@ -0,0 +1,15 @@
+package app.trip.exceptions.authentication;
+
+public class UserAlreadyExistsException extends Exception{
+
+	public UserAlreadyExistsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserAlreadyExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
