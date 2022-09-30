858c08c07ae0371461ec0709a025fffe99c027b6
Sbsharma0897 <sbsharma0897@gmail.com>
2022-10-01 01:36:44 +0530

    FeedBack module done

--- OnlineTripManagementSystemApplication/src/main/java/app/trip/models/User.java
+++ OnlineTripManagementSystemApplication/src/main/java/app/trip/models/User.java
@@ -1,9 +1,13 @@
 package app.trip.models;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -33,4 +37,8 @@ public class User {
 	@Pattern(regexp = "[A-Za-z0-9]{6,12}",message = "Password must be 6 to 8 characters and must have at least 1 alphabate and 1 number")
 	@NotNull @NotBlank @NotEmpty
 	private String password;
+	
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+	private List<Feedback> feedbacks;
 }
