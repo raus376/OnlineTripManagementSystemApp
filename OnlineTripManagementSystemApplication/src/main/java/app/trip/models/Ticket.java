3b3a0fcf35c43f851b8f9c63278d419f09032061
Kunal Ladhani <k.ladhani1@gmail.com>
2022-10-01 17:39:01 +0530

    resolved ticketstatus and createticket error

--- OnlineTripManagementSystemApplication/src/main/java/app/trip/models/Ticket.java
+++ OnlineTripManagementSystemApplication/src/main/java/app/trip/models/Ticket.java
@@ -15,11 +15,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
