a66c08417d66916fa7cf75215dde5e383cb44bd9
Kunal Ladhani <k.ladhani1@gmail.com>
2022-10-02 20:11:05 +0530

    Project Completed and Deployed

--- OnlineTripManagementSystemApplication/src/main/java/app/trip/OnlineTripManagementSystemApplication.java
+++ OnlineTripManagementSystemApplication/src/main/java/app/trip/OnlineTripManagementSystemApplication.java
@@ -3,6 +3,10 @@ package app.trip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@EnableSwagger2
 @SpringBootApplication
 public class OnlineTripManagementSystemApplication {
 
